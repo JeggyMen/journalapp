49b03f772635efd211d1bf93ac156cba252e9c15
Johnneil Mendoza <johnneilmendoza@gmail.com>
2024-07-11 16:51:05 +0800

    implementation of user story 2 edit and update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
     def new
         @category = Category.new
     end
-    
+
 
     def create
         @category = Category.new(category_params)
@@ -18,10 +18,24 @@ class CategoriesController < ApplicationController
         end
     end
     
+    def edit
+    end
+
+    def update
+        respond_to do |format|
+            if @category.update(category_params)
+                format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
+                format.json { render :show, status: :ok, location: @category }
+            else 
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @categ.errors, status: :unprocessable_entity }
+            end
+        end    
+    end
+
     private
     def category_params
       params.require(:category).permit(:name, :description)
     end
   end
-
   
