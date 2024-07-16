e09498de502c5f008946222fe2471bc0983f1651
Johnneil Mendoza <johnneilmendoza@gmail.com>
2024-07-16 18:39:57 +0800

    implementation of user story 4 create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+   
+   def index
+     @tasks = @category.tasks
+   end
+   
+   def new
+     @tasks = @category.tasks.build
+   end
+
+   def create
+     @tasks = @category.tasks.build(task_params)
+
+     if @task.save
+        redirect_to category_task_path(@category, @task), notice: 'Succesfully created new task.'
+     else
+        render :new, status: :unprocessable_entity
+     end
+   end
+
 end
