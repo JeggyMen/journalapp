2c7afaf5e727fbd439ff8e197d756039f6c82a78
Johnneil Mendoza <johnneilmendoza@gmail.com>
2024-07-17 17:03:31 +0800

    test for user story 6 show

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,34 +1,40 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    # test "the truth" do
+    #   assert true
+    # end
 
-  setup do
-    @category = categories(:one)
-    @task = tasks(:one)
-  end
-
-  test "should get index" do
-    get category_tasks_path(@category)
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_category_task_path(@category)
-    assert_response :success
-  end
-
-  test "should create task for a specific category" do
-    assert_difference("Task.count") do
-      post category_tasks_path(@category), params: {task: { body: "New task is added."}}
+    setup do
+      @category = categories(:one)
+      @task = tasks(:one)
     end
-    assert_redirected_to category_task_path(@category, Task.last)
-   end
 
-   test "should get edit" do
-    get edit_category_task_path(@category, @task)
-    assert_response :success
- end
+    test "should get index" do
+      get category_tasks_path(@category)
+      assert_response :success
+    end
+
+    test "should get new" do
+      get new_category_task_path(@category)
+      assert_response :success
+    end
+
+    test "should create task for a specific category" do
+      assert_difference("Task.count") do
+        post category_tasks_path(@category), params: {task: { body: "New task is added."}}
+    end
+      assert_redirected_to category_task_path(@category, Task.last)
+    end
+
+    test "should get edit" do
+      get edit_category_task_path(@category, @task)
+      assert_response :success
+    end
+
+    test "should show task" do
+      get category_task_path(@category, @task) 
+      assert_response :success
+    end
+ 
 end
